6af922adcbbacbf6b8e59862b1ef8b6375a1df8a
Ulquiorra <gegggeeh@gmail.com>
2025-04-04 22:08:24 +0200

    Fix mongoose.connect() type error

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -33,9 +33,7 @@ async function connectDB() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose.connect(MONGODB_URI, opts);
   }
 
   try {
