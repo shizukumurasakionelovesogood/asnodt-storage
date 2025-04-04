7bdf5e55da66ddc875e071e13b8e29da8b7d8e02
Ulquiorra <gegggeeh@gmail.com>
2025-04-04 22:23:50 +0200

    fix: добавлена поддержка статической генерации для next-intl

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,10 @@
 import { getRequestConfig } from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`@/messages/${locale}.json`)).default
-})); 
+export default getRequestConfig(async ({ locale }) => {
+  setRequestLocale(locale);
+  
+  return {
+    messages: (await import(`@/messages/${locale}.json`)).default
+  };
+}); 
